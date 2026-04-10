5f3d2a45154c3cb444070dea62bc949c0e0444dd
alikouhestani <90757950+alikouhestani@users.noreply.github.com>
2026-04-10 13:28:15 +0100

    initial set up for stubs repository

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -1,9 +1,9 @@
-package uk.gov.hmrc.crsfatcamanualsubmissionstubs.config
+package config
 
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 
 @Singleton
-class AppConfig @Inject()(config: Configuration):
+class AppConfig @Inject() (config: Configuration):
 
   val appName: String = config.get[String]("appName")
